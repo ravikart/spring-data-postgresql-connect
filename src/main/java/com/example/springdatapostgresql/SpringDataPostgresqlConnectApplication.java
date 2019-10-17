83f33461d10f796c47c7dcf293eb485352a0bfd0
Ravi Kumar Karthikeyan <Ravi Kumar Karthikeyan>
2019-10-17 07:47:06 +0530

    refs #1 spring-data project set up commit.

---
+++ src/main/java/com/example/springdatapostgresql/SpringDataPostgresqlConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatapostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataPostgresqlConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataPostgresqlConnectApplication.class, args);
+	}
+
+}
